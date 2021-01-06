1a69fbb2cd686265b32ed28643c9c27da40e223f
wcyjames <wcy_james@outlook.com>
2021-01-06 15:06:44 +0800

    e

--- pointnet2_partseg.py
+++ pointnet2_partseg.py
@@ -38,13 +38,14 @@ class PointNet2SSGPartSeg(nn.Module):
             l0_pos = x
             l0_feat = x
         # Set Abstraction layers
+
         profiler.start()
         l1_pos, l1_feat = self.sa_module1(l0_pos, l0_feat)  # l1_feat: [B, N, D]
+        l2_pos, l2_feat = self.sa_module2(l1_pos, l1_feat)
+        l3_pos, l3_feat = self.sa_module3(l2_pos, l2_feat)  # [B, N, C], [B, D]
         profiler.stop()
         print(profiler.output_text(unicode=True, color=True))
 
-        l2_pos, l2_feat = self.sa_module2(l1_pos, l1_feat)
-        l3_pos, l3_feat = self.sa_module3(l2_pos, l2_feat)  # [B, N, C], [B, D]
         # Feature Propagation layers
         l2_feat = self.fp3(l2_pos, l3_pos, l2_feat, l3_feat.unsqueeze(1))  # l2_feat = [B, D, N]
         l1_feat = self.fp2(l1_pos, l2_pos, l1_feat, l2_feat.permute(0, 2, 1))
