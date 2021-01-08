578cffba7271068e76ffbb4504922750166eeff6
wcyjames <wcy_james@outlook.com>
2021-01-08 16:43:23 +0800

    e

--- pointnet2.py
+++ pointnet2.py
@@ -148,12 +148,17 @@ class PointNetConv(nn.Module):
             self.bn.append(nn.BatchNorm2d(sizes[i]))
 
     def forward(self, nodes):
-        shape = nodes.mailbox['agg_feat'].shape
-        h = nodes.mailbox['agg_feat'].view(self.batch_size, -1, shape[1], shape[2]).permute(0, 3, 1, 2)
-        for conv, bn in zip(self.conv, self.bn):
-            h = conv(h)
-            h = bn(h)
-            h = F.relu(h)
+        profiler.start()
+        for i in range(50):
+            shape = nodes.mailbox['agg_feat'].shape
+            h = nodes.mailbox['agg_feat'].view(self.batch_size, -1, shape[1], shape[2]).permute(0, 3, 1, 2)
+            print(h.shape)
+            for conv, bn in zip(self.conv, self.bn):
+                h = conv(h)
+                h = bn(h)
+                h = F.relu(h)
+        profiler.stop()
+        print(profiler.output_text(unicode=True, color=True, show_all=True))
         h = torch.max(h, 3)[0]
         feat_dim = h.shape[1]
         h = h.permute(0, 2, 1).reshape(-1, feat_dim)
@@ -199,13 +204,7 @@ class SAModule(nn.Module):
 
         centroids = self.fps(pos)
         g = self.frnn_graph(pos, centroids, feat)
-        profiler.start()
-        for i in range(50):
-            #g.update_all(self.message, fn.mean('agg_feat', 'h'))
-            g.update_all(self.message, self.conv)
-        profiler.stop()
-        print(profiler.output_text(unicode=True, color=True, show_all=True))
-        # g.update_all(self.message, self.conv)
+        g.update_all(self.message, self.conv)
 
         mask = g.ndata['center'] == 1
         pos_dim = g.ndata['pos'].shape[-1]
