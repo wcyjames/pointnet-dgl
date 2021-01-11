9f92d87d7d8f9c9fbf0e466c6f3d4f21cdd7ef1a
wcyjames <wcy_james@outlook.com>
2021-01-11 18:00:45 +0800

    e

--- debug.py
+++ debug.py
@@ -60,10 +60,10 @@ class PNConv(nn.Module):
 
         shape = nodes.mailbox['agg_feat'].shape
         h = nodes.mailbox['agg_feat'].view(self.batch_size, -1, shape[1], shape[2]).permute(0, 3, 1, 2) # torch.Size([16, 6, 512, 32])
-        for conv, bn in zip(self.conv, self.bn):
-            h = conv(h)
-            h = bn(h)
-            h = F.relu(h)
+        # for conv, bn in zip(self.conv, self.bn):
+        #     h = conv(h)
+        #     h = bn(h)
+        #     h = F.relu(h)
 
         #profiler.start()
         #for i in range(50):
@@ -77,9 +77,9 @@ class PNConv(nn.Module):
         # profiler.stop()
         # print(profiler.output_text(unicode=True, color=True, show_all=True))
 
-        h = torch.max(h, 3)[0]
-        feat_dim = h.shape[1]
-        h = h.permute(0, 2, 1).reshape(-1, feat_dim)
+        # h = torch.max(h, 3)[0]
+        # feat_dim = h.shape[1]
+        # h = h.permute(0, 2, 1).reshape(-1, feat_dim)
         return {'new_feat': h}
 
 
