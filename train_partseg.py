a7284c35f532506bf26b40d18a44957ee621534e
wcyjames <wcy_james@outlook.com>
2021-02-01 16:30:34 +0800

    timing

--- train_partseg.py
+++ train_partseg.py
@@ -198,8 +198,16 @@ def paint(batched_points):
 best_test_miou = 0
 best_test_per_cat_miou = 0
 
+times = []
 for epoch in range(args.num_epochs):
+    start = time.time()
     data, preds, AvgLoss, AvgAcc, training_time = train(net, opt, scheduler, train_loader, dev)
+    end = time.time()
+    t = end - start
+    print("training time:", t)
+    times.append(t)
+    if epoch == 2:
+        print("3 epochs avg:", sum(times) / len(times))
     if (epoch + 1) % 5 == 0:
         print('Epoch #%d Testing' % epoch)
         test_miou, test_per_cat_miou = evaluate(net, test_loader, dev, (epoch + 1) % 5 ==0)
